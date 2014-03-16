0c05d406e96337a77aaa55032e4f32fb5107c6a9
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-03-16 16:14:18 -0300

    Point to the proper api endpoint.

--- frontend/assets/scripts/landing.js
+++ frontend/assets/scripts/landing.js
@@ -5,13 +5,17 @@ $(function () {
 
     $.ajax({
         type: "POST",
-        url: "http://api.moodsy.com/beta/signup",
+        url: "http://api.moodsy.me/subscribers/signup",
         data: {
             "email": $('#email').val()
         },
         success: function() {
+            // console.log("sucess");
             $('#dev-subscribe').html('Thank You!');
             $('#dev-subscribe').removeClass('disabled');
+        },
+        error: function() {
+            // console.error("Submission failure");
         }
         // TODO: What about errors?
     });
