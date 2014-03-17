8c5e5557043e22fa5840f8bf398ef19df88b4c8c
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-03-17 00:01:13 -0300

    Cleaned up settings-loading code.

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,19 +7,24 @@ from endpoints import SubscriberSignUp, SubscribersListing, DummyOk, DummyError
 import logging
 import logging.config
 import json
+import os
 import socket
 
 ### Configure Logging ###
 
-logging_config = json.load(open('logging.json', 'r'))
+script_path = os.path.abspath(os.path.dirname(__file__))
+
+logging_config_path = os.path.join(script_path, 'logging.json')
+db_config_path = os.path.join(script_path, 'db.json')
+
+logging_config = json.load(open(logging_config_path, 'r'))
 logging.config.dictConfig(logging_config)
 
-
-logger = logging.getLogger(__name__)
-
-db_config = json.load(open('db.json', 'r'))
+db_config = json.load(open(db_config_path, 'r'))
 db_string = '{protocol}://{user}:{password}@{host}:{port}/{name}'. \
     format(**db_config)
+
+logger = logging.getLogger(__name__)
 
 logger.info("Initializing schema: {}".format(db_string))
 init_schema(db_string)
