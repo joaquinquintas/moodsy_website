671e85bde452b66cff4e5f0ce8bc820ac4d03cd7
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-05-10 20:03:30 -0300

    Completely generate pages server-side.
    Stop doing the API calls client side and send full pages to web clients
    instead.
    
    We'll be using tornado server-side to do this, but will retain the jade and
    scss templating for working with the templates on the development side.

--- Gruntfile.coffee
+++ Gruntfile.coffee
@@ -5,25 +5,16 @@ module.exports = (grunt) ->
         cwd: "src"
         src: [
           "**"
-          "!**/*.coffee"
           "!**/*.scss"
           "!**/*.jade"
         ]
-        dest: "build"
+        dest: "build/static"
         expand: true
 
     clean:
       build:
         src: ["build"]
 
-    coffee:
-      glob_to_multiple:
-        expand: true
-        flatten: true
-        cwd: 'src'
-        src: ['*.coffee']
-        dest: 'build/'
-        ext: '.js'
     jade:
       compile:
         options:
@@ -38,10 +29,6 @@ module.exports = (grunt) ->
         ]
 
     watch:
-      scripts:
-        files: ["src/**/*.coffee", "src/**/*.js"]
-        tasks: ["scripts", "copy"]
-
       jade:
         files: "src/**/*.jade"
         tasks: ["jade", "copy"]
@@ -53,31 +40,16 @@ module.exports = (grunt) ->
       copy:
         files: [
           "src/**"
-          "!src/**/*.coffee"
           "!src/**/*.jade"
         ]
 
-    connect:
-      server:
-        options:
-          port: 4000
-          base: "build"
-          hostname: "*"
-
-    bowercopy:
-      libs:
-        options:
-          destPrefix: "build"
-
-        files:
-          "moment.js": "moment/min/moment.min.js"
     sass:
       dist:
         files: [
           expand: true
           cwd: "src"
           src: ["**/*.scss"]
-          dest: "build"
+          dest: "build/static"
           ext: ".css"
         ]
 
@@ -86,17 +58,13 @@ module.exports = (grunt) ->
   require("load-grunt-tasks") grunt
 
   #define tasks
-  grunt.registerTask "scripts", "Compile scripts only.", ["coffee"]
   grunt.registerTask "build", "Compiles everything and copy into build.", [
     "clean"
     "copy"
-    "bowercopy"
     "sass"
-    "scripts"
     "jade"
   ]
   grunt.registerTask "serve", "Serve locally and update file on change.", [
     "build"
-    "connect"
     "watch"
   ]
---
+++ moodsy_www/main.py
@@ -0,0 +1,68 @@
+import os
+import random
+from concurrent.futures import ThreadPoolExecutor
+
+import arrow
+import requests
+import tornado.ioloop
+from tornado.web import RequestHandler, Application, url
+from tornado.gen import coroutine
+
+# TODO: These will have to be configurable.
+PORT = 5000
+MOOD_ENDPOINT = 'http://api.moodsy.me/2/public/post/{mood_id}/'
+SOURCE_PATH = os.path.dirname(os.path.abspath(__name__))
+TEMPLATE_PATH = os.path.join(SOURCE_PATH, 'build')
+STATIC_PATH = os.path.join(SOURCE_PATH, 'build', 'static')
+
+# We'll use a thread pool to handle up to four requests *per instance* at a
+# time.
+thread_pool = ThreadPoolExecutor(8)
+
+
+class MainPageHandler(RequestHandler):
+
+    template = os.path.join(TEMPLATE_PATH, 'index.html')
+
+    def get(self):
+        number = random.randint(0, 3)
+        self.render(self.template, number=number)
+
+
+class MoodPageHandler(RequestHandler):
+
+    template = os.path.join(TEMPLATE_PATH, 'mood.html')
+
+    def async_get(self, mood_id):
+        url = MOOD_ENDPOINT.format(mood_id=mood_id)
+        resp = requests.get(url)
+        if resp.status_code != 200:
+            # TODO: We need a design for the 404 page.
+            self.set_status(404)
+            self.write('No such mood')
+            return
+
+        mood = resp.json()
+        moodsies = [m['user_fname'] for m in mood['moodsies']['moodsies']]
+        mood['post']['moment'] = arrow.get(
+            mood['post']['datetime'],
+            'YYYY-MM-DD HH:mm:ss Z'
+        ).humanize()
+
+        self.render(self.template, mood=mood, moodsies=moodsies)
+
+    @coroutine
+    def get(self, mood_id):
+        yield thread_pool.submit(self.async_get, mood_id)
+
+
+application = Application([
+    url(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': STATIC_PATH}),
+    url(r"/?", MainPageHandler, name='main'),
+    url(r"/m/(.{10})/?", MoodPageHandler, name='mood'),
+], debug=True)
+
+if '__main__' == __name__:
+    print('Starting server on {}'.format(PORT))
+    application.listen(PORT)
+    tornado.ioloop.IOLoop.instance().start()
